9be3040497e9f79f8553b58e3a89ea0a52f89d10
Brett Turner Francis <brettturnerfrancis@gmail.com>
2020-08-30 17:24:34 -0500

    commit static build files for demo

---
+++ build/precache-manifest.af814508e401b102f8c93dff8fd2a0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc59331c39c55d79c1cd989ee79988a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9494bc35506a0c8cad7c",
+    "url": "/static/css/main.9e202b85.chunk.css"
+  },
+  {
+    "revision": "3ff1ad9907f1b3037a8b",
+    "url": "/static/js/2.6e0625e6.chunk.js"
+  },
+  {
+    "revision": "b51a2958d3ab2a013035e7cb135ca861",
+    "url": "/static/js/2.6e0625e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9494bc35506a0c8cad7c",
+    "url": "/static/js/main.1a318fd8.chunk.js"
+  },
+  {
+    "revision": "b183dea5c6dc9deffeed",
+    "url": "/static/js/runtime-main.ccdc9b21.js"
+  }
+]);
