97624d2da48dcd1d1d6e2eb8f00a26f9d0dc1f99
Brett Turner Francis <brettturnerfrancis@gmail.com>
2020-08-30 22:41:32 -0500

    recipe links to file

--- src/Recipe/Recipe.jsx
+++ src/Recipe/Recipe.jsx
@@ -3,10 +3,10 @@ import './Recipe.css';
 
 function Recipe({recipe}) {
     return (
-        <div className="recipe">
+        <a className="recipe" target="_blank" href={recipe.location}>
             <div data-id="recipe-name" className="recipe__name">{recipe.name}</div>
             <div data-id="recipe-source" className="recipe__source">{recipe.source}</div>
-        </div>
+        </a>
     )
 }
 
--- src/Recipe/Recipe.test.js
+++ src/Recipe/Recipe.test.js
@@ -19,13 +19,18 @@ describe('recipe', () => {
 
     it('displays the title', () => {
         const title = instance.findByProps({'data-id':'recipe-name'});
-
         expect(title.props.children).toBe(mockRecipe.name);
     })
 
     it('displays the recipe source', () => {
         const source = instance.findByProps({'data-id':'recipe-source'});
-
         expect(source.props.children).toBe(mockRecipe.source);
+    })
+
+    it('links to the related file', () => {
+        const link = instance.children[0]; 
+        expect(link.type).toBe('a')
+        expect(link.props.target).toBe('_blank');
+        expect(link.props.href).toBe(mockRecipe.location)
     })
 });
